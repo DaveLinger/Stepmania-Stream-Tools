1287a2b4b08bb286824b803c5df434c4b242325f
Dave <22969871+DaveLinger@users.noreply.github.com>
2020-07-20 16:57:37 -0400

    php env var support
    Added env variable support for php. Removed external mysql access.

--- request_list/includes/config.php
+++ request_list/includes/config.php
@@ -1,20 +1,38 @@
 <?php
 
-//Your database host. "localhost" if you are running the DB on the same machine as the web server.
-define('dbhost', 'mysql');
-//Your database username.
-define('dbuser', 'sw_user');
-//Your database password.
-define('dbpass', 'Y71o8QcQNn');
-//Your database name.
-define('db', 'sm_software');
+//Check for docker variables to use first
+
+if(getenv("MYSQL_DATABASE") != ""){
+    $dbname = getenv("MYSQL_DATABASE");
+    define('dbhost', 'mysql');
+    define('db', $dbname);
+}else{
+    define('dbhost', '');
+    define('db', '');
+}
+
+if(getenv("MYSQL_USER") != ""){
+    $dbuser = getenv("MYSQL_USER");
+    define('dbuser', $dbuser);
+}else{
+    define('dbuser', '');
+}
+
+if(getenv("MYSQL_PASSWORD") != ""){
+    $dbpass = getenv("MYSQL_PASSWORD");
+    define('dbpass', $dbpass);
+}else{
+    define('dbpass', '');
+}
+
+if(getenv("SECRET_KEY") != ""){
+    $security_key = getenv("SECRET_KEY");
+}else{
+    $security_key = "";
+}
 
 //List of banned songs, comma separated. This is going to be replaced by database flags soon. Example: Array(69,420,80085);
 $banned = Array();
 $GLOBALS["banned"] = $banned;
-
-//Security key. Set this to anything. All incoming requests (like from moobot) will have to include this key or they'll be discarded.
-//This way people can't hit your endpoints directly without permission.
-$security_key = "type-literally-anything-here";
 
 ?>
