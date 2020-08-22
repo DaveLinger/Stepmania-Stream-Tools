fc2b64344203d7e6aad184bd643a6b01a77f7dec
MrTwinkles47 <64985827+MrTwinkles47@users.noreply.github.com>
2020-08-22 18:32:27 -0400

    doth quotetheth the raveneth

--- song-scraper/config.php
+++ song-scraper/config.php
@@ -1,28 +1,28 @@
 <?php
 
-//Your database host. "localhost" if you are running the DB on the same machine as the web server.
-define("dbhost", "");
+//Your database host. 'localhost' if you are running the DB on the same machine as the web server.
+define('dbhost', '');
 //Your database username.
-define("dbuser", "");
+define('dbuser', '');
 //Your database password.
-define("dbpass", "");
+define('dbpass', '');
 //Your database name.
-define("db", "");
+define('db', '');
 
 //Your path to StepMania's song cache folder.
-$cacheDir = "C:/Users/Admin/AppData/Roaming/StepMania 5.1/Cache/Songs";
+$cacheDir = 'C:/Users/Admin/AppData/Roaming/StepMania 5.1/Cache/Songs';
 
 //location of stepmania local profile folder
-$profileDir = "C:/Users/Admin/AppData/Roaming/StepMania 5.1/Save/LocalProfiles";
+$profileDir = 'C:/Users/Admin/AppData/Roaming/StepMania 5.1/Save/LocalProfiles';
 
 //location of StepMania songs folder
-$songsDir = "D:/StepMania 5.1/Songs";
+$songsDir = 'D:/StepMania 5.1/Songs';
 
 //Target url for uploading banner images to the server. This directory MUST exist before uploading banners.
-$target_url = "https://domain.tld/sm5/banners.php";
+$target_url = 'https://domain.tld/sm5/banners.php';
 
 //Security key. Set this to anything. All incoming requests (like from moobot) will have to include this key or they'll be discarded.
 //This way people can't hit your endpoints directly without permission.
-$security_key = "any-secret-here";
+$security_key = 'any-secret-here';
 
 ?>
